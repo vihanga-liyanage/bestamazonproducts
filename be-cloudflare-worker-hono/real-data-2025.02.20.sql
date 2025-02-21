e145c470d70a5a6beeca9009399e4a149d59e960
Vihanga Liyanage <vihangaliyanage007@gmail.com>
2025-02-20 22:05:54 -0600

    Add real data, change logo and name

---
+++ be-cloudflare-worker-hono/real-data-2025.02.20.sql
@@ -0,0 +1,13 @@
+INSERT INTO products (title, description, price, image_url, affiliate_url) VALUES
+('Hummingbird Feeders for Outdoors', 'Hummingbird Feeders for Outdoors', 11.99, 'https://m.media-amazon.com/images/I/71nYNI5JjHL._AC_SX679_.jpg', 'https://amzn.to/3Qrg2T5'),
+('VVG Starlink Cable for Starlink Actuated Gen 2', 'VVG Starlink Cable for Starlink Actuated Gen 2', 18.39, 'https://m.media-amazon.com/images/I/71VabR1WS9L._SX522_.jpg', 'https://amzn.to/4hPUrA6'),
+('Water Dental Flosser', 'Water Dental Flosser', 39.99, 'https://m.media-amazon.com/images/I/71QBVFNCVoL._AC_SX679_.jpg', 'https://amzn.to/3EXLxBK'),
+('Rechargeable Hearing Amplifiers for Seniors', 'Rechargeable Hearing Amplifiers for Seniors', 78.99, 'https://m.media-amazon.com/images/I/515jttgVejL._SX522_.jpg', 'https://amzn.to/4hJ0pm4'),
+('Rechargeable Hearing Amplifiers for Seniors', 'Rechargeable Hearing Amplifiers for Seniors', 57.99, 'https://m.media-amazon.com/images/I/41AdNg-a6BL._SX522_.jpg', 'https://amzn.to/4hJogSL'),
+('Hidden Camera Detector - 2025', 'Hidden Camera Detector - 2025', 49.98, 'https://m.media-amazon.com/images/I/41EpjYoJxAL._AC_SX466_.jpg', 'https://amzn.to/4bbhse8'),
+('Natural Plant Lip Balm for Dark Lips', 'Natural Plant Lip Balm for Dark Lips', 7.99, 'https://m.media-amazon.com/images/I/617m0EcGA5L._SX679_.jpg', 'https://amzn.to/3X9oj1X'),
+('TV Antenna Indoor Outdoor', 'TV Antenna Indoor Outdoor', 49.99, 'https://m.media-amazon.com/images/I/61Cu4CVxElL._AC_SX466_.jpg', 'https://amzn.to/3QvUNzL'),
+('Smart Bluetooth Tracker 4 pack', 'Smart Bluetooth Tracker 4 pack', 36.99, 'https://m.media-amazon.com/images/I/71+AvcYeecL._AC_SX679_.jpg', 'https://amzn.to/43bm3ex'),
+('Advanced Eyelash Serum', 'Advanced Eyelash Serum', 24.99, 'https://m.media-amazon.com/images/I/61bpphozj7L._SX466_.jpg', 'https://amzn.to/3QsFXdd'),
+('Fast Heating Mug warmer', 'Fast Heating Mug warmer', 25.99, 'https://m.media-amazon.com/images/I/71kXSL+GJ6L._AC_SX679_.jpg', 'https://amzn.to/3ELDOa9'),
+('Toilet Seat Covers (60 pack)', 'Toilet Seat Covers (60 pack)', 8.49, 'https://m.media-amazon.com/images/I/7190IYgXhuL._AC_SX679_.jpg', 'https://amzn.to/3QqNIAz');
--- fe-cloudflare-pages-react/src/components/Header.tsx
+++ fe-cloudflare-pages-react/src/components/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../styles/Header.css';
+import logo from '../assets/sp-logo.png';
 
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div className="logo">Amazon Affiliate</div>
+      <div className="logo-container">
+        <img src={logo} alt="Smarter Picks Logo" className="logo" />
+        <h1 className="site-title">Smarter Picks</h1>
+      </div>
       <nav className="nav">
         <a href="/">Home</a>
         <a href="/deals">Deals</a>
